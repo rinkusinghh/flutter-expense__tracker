f95990cff0da7f5db3514ee896d0baa71fd7a44e
rinku singh <rinkusingh05011999@gmail.com>
2022-10-30 05:31:53 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense tracker',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense tracker'),
+      ),
+      body: Center(
+        child: Text('Childrens!'),
+      ),
+    );
+  }
+}
