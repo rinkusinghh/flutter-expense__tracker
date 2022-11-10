8a12f6c075931869c6a935133c2c92eff9fa198a
rinku singh <rinkusingh05011999@gmail.com>
2022-11-10 21:12:57 +0530

    Splitting the app into widgets

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,54 @@
+import "package:flutter/material.dart";
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transaction;
+
+  TransactionList(this.transaction);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: transaction.map(
+        (transaction) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.orange,
+                    width: 2,
+                  )),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\₹${transaction.amount.toString()}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transaction.title,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat().format(transaction.date),
+                      style: TextStyle(color: Colors.grey, fontSize: 16),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+      ).toList(),
+    );
+  }
+}
