209a384e4ee909747dfd69771a6d5b5f41c4f431
rinku singh <rinkusingh05011999@gmail.com>
2022-12-08 17:40:26 +0530

    Rendering alternative content-1

--- lib/main.dart
+++ lib/main.dart
@@ -61,6 +61,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // ),
   ];
 
+  bool _showChart = false;
+
   List<Transaction> get _recentTransactions {
     return _userTransaction.where((tx) {
       return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
@@ -117,20 +119,33 @@ class _MyHomePageState extends State<MyHomePage> {
           // enum:- Is a couple of different values!
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            // Chart text!
-            Container(
-                height: (MediaQuery.of(context).size.height -
-                        MediaQuery.of(context).padding.top -
-                        appBar.preferredSize.height) *
-                    0.3,
-                child: Chart(_recentTransactions)),
-            Container(
-              height: (MediaQuery.of(context).size.height -
-                      MediaQuery.of(context).padding.top -
-                      appBar.preferredSize.height) *
-                  0.7,
-              child: TransactionList(_userTransaction, _deleteTransaction),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Show Chart'),
+                Switch(
+                    value: _showChart,
+                    onChanged: (val) {
+                      setState(() => _showChart = val);
+                    })
+              ],
             ),
+            // Chart text!
+            _showChart
+                ? Container(
+                    height: (MediaQuery.of(context).size.height -
+                            MediaQuery.of(context).padding.top -
+                            appBar.preferredSize.height) *
+                        0.3,
+                    child: Chart(_recentTransactions))
+                : Container(
+                    height: (MediaQuery.of(context).size.height -
+                            MediaQuery.of(context).padding.top -
+                            appBar.preferredSize.height) *
+                        0.7,
+                    child:
+                        TransactionList(_userTransaction, _deleteTransaction),
+                  ),
           ],
         ),
       ),
