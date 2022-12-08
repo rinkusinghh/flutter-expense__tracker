b409fad861efa79c1361023922f3f97dc6a7ad77
rinku singh <rinkusingh05011999@gmail.com>
2022-12-09 05:23:50 +0530

    Respecting the softkeyboard Insets

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -49,50 +49,56 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: _titleController,
-              keyboardType: TextInputType.number, //IOS:- decimal:true
-              onSubmitted: (_) => _submitBtn,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => _submitBtn,
-            ),
-            Container(
-              height: 80,
-              child: Row(children: [
-                Expanded(
-                    child: Text(_selectedDate == null
-                        ? 'No Chosen Date'
-                        : 'Picked Date${DateFormat.yMd().format(_selectedDate)}')),
-                TextButton(
-                  onPressed: _presentDatePicker,
-                  child: Text('Chosen Date',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                )
-              ]),
-            ),
-            ElevatedButton(
-              onPressed: _submitBtn,
-              child: Text(
-                "Add transaction",
-                style: TextStyle(fontWeight: FontWeight.w500),
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+              top: 10,
+              left: 10,
+              right: 10,
+              bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: _titleController,
+                keyboardType: TextInputType.number, //IOS:- decimal:true
+                onSubmitted: (_) => _submitBtn,
               ),
-              style: TextButton.styleFrom(
-                textStyle: TextStyle(fontSize: 18),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitBtn,
               ),
-            ),
-          ],
+              Container(
+                height: 80,
+                child: Row(children: [
+                  Expanded(
+                      child: Text(_selectedDate == null
+                          ? 'No Chosen Date'
+                          : 'Picked Date${DateFormat.yMd().format(_selectedDate)}')),
+                  TextButton(
+                    onPressed: _presentDatePicker,
+                    child: Text('Chosen Date',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  )
+                ]),
+              ),
+              ElevatedButton(
+                onPressed: _submitBtn,
+                child: Text(
+                  "Add transaction",
+                  style: TextStyle(fontWeight: FontWeight.w500),
+                ),
+                style: TextButton.styleFrom(
+                  textStyle: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
